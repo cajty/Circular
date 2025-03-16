5518d8ca340a7303564a34afe6feed2e7a3588b4
ayoub belyamane <fdshjgjgffjhd@gmail.com>
2025-03-16 13:19:34 +0100

    remove that material for sale

--- src/main/java/org/ably/circular/offre/Offre.java
+++ src/main/java/org/ably/circular/offre/Offre.java
@@ -1,14 +1,8 @@
 package org.ably.circular.offre;
-
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.ably.circular.enterprise.Enterprise;
-import org.ably.circular.recyclableMaterial.Material;
-import org.ably.circular.transaction.Transaction;
 
-import java.util.Set;
 
 @Getter
 @Setter
@@ -16,13 +10,4 @@ import java.util.Set;
 @Table(name = "offre")
 public class Offre {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @ManyToMany(mappedBy = "")
-    private Set<Material> materials;
-
-    @ManyToOne
-    private Enterprise enterprise;
-}
