5f27445404b917bb43c4d7e7c0f8f73134903e23
ayoub belyamane <fdshjgjgffjhd@gmail.com>
2025-03-27 19:41:00 +0100

    last commit

--- src/main/java/org/ably/circular/config/SecurityConfiguration.java
+++ src/main/java/org/ably/circular/config/SecurityConfiguration.java
@@ -34,17 +34,15 @@ public class SecurityConfiguration {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(
-                        "/**"
-//                    "/auth/**",
-//                    "/error",
-//                    "/swagger-ui/**",
-//                    "/swagger-ui.html",
-//                    "/v2/api-docs",
-//                    "/v3/api-docs",
-//                    "/v3/api-docs/**",
-//                    "/swagger-resources",
-//                    "/swagger-resources/**",
-//                    "/webjars/**"
+                 "/auth/**",
+                    "/swagger-ui/**",
+                    "/swagger-ui.html",
+                    "/v2/api-docs",
+                    "/v3/api-docs",
+                    "/v3/api-docs/**",
+                    "/swagger-resources",
+                    "/swagger-resources/**",
+                    "/webjars/**"
                 ).permitAll()
                 .anyRequest().authenticated()
             )
